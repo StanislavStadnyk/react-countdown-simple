80701861ff086083658249e7642663adc9e89f8a
StanislavStadnyk <stanislav.stadnyk@gmail.com>
2022-08-29 10:34:33 +1000

    Added link tp package.json for issues

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
-import {ReactElement} from "react";
+import { ReactElement } from 'react';
 
 export type TFormatType =
-  | "d_h_m_s"
-  | "D_H_M_S"
-  | "dd_hh_mm_ss"
-  | "DD_HH_MM_SS"
+  | 'd_h_m_s'
+  | 'D_H_M_S'
+  | 'dd_hh_mm_ss'
+  | 'DD_HH_MM_SS'
   | undefined;
 
 export type TRenderProps = (renderProps: {
@@ -15,8 +15,8 @@ export type TRenderProps = (renderProps: {
 }) => ReactElement;
 
 export enum EDateTime {
-  D_H_M_S = "D_H_M_S",
-  d_h_m_s = "d_h_m_s",
-  DD_HH_MM_SS = "DD_HH_MM_SS",
-  dd_hh_mm_ss = "dd_hh_mm_ss",
+  D_H_M_S = 'D_H_M_S',
+  d_h_m_s = 'd_h_m_s',
+  DD_HH_MM_SS = 'DD_HH_MM_SS',
+  dd_hh_mm_ss = 'dd_hh_mm_ss',
 }
